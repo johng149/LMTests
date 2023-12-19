bb6d5bdc383026db7207217fec7c0b5dac5458e1
johng149 <jg843@cornell.edu>
2023-12-19 08:26:44 -0500

    Working on data loading

--- daglm/utils/data.py
+++ daglm/utils/data.py
@@ -1,4 +1,47 @@
 import torch
+from torch.utils.data import Dataset
+import os
+
+class TranslateDataset(Dataset):
+
+    def __init__(self, data_dir, data_file):
+        self.data_dir = data_dir
+        self.data_file = data_file
+        self.load_data()
+
+    def load_data(self):
+        print(f"Loading data from {os.path.join(self.data_dir, self.data_file)}")
+        self.ens, self.zhs = torch.load(os.path.join(self.data_dir, self.data_file))
+        self.num_samples, _ = self.ens.shape # num_samples, seq_len
+
+    def __len__(self):
+        return self.num_samples
+    
+    def __getitem__(self, idx):
+        return self.ens[idx], self.zhs[idx]
+    
+def collate_fn(batch, pad_idx=65000):
+    """
+    Collate function for the TranslateDataset.
+    For each batch, the English and Chinese samples are stacked into tensors.
+    And then the columns that only consist of padding tokens are removed
+
+    Args:
+        batch (list): The batch of samples.
+
+    Returns:
+        torch.Tensor: The batch of English samples.
+        torch.Tensor: The batch of Chinese samples.
+    """
+    ens, zhs = zip(*batch)
+    ens = torch.stack(ens)
+    zhs = torch.stack(zhs)
+
+    # remove columns that only consist of padding tokens
+    ens = ens[:, (ens != pad_idx).any(dim=0)]
+    zhs = zhs[:, (zhs != pad_idx).any(dim=0)]
+    
+    return ens, zhs
 
 def process_data(samples, pad_idx, factor):
     """
