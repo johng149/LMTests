ce9e01de309b22a0875ece7754bf9d616ebe2feb
johng149 <jg843@cornell.edu>
2023-12-19 21:03:46 -0500

    Working on full training loop

--- daglm/losses/dag_loss.py
+++ daglm/losses/dag_loss.py
@@ -74,6 +74,7 @@ def dag_loss_raw(targets, transition_matrix, emission_probs):
     # also we need to tranpose emission_probs so it is vocab_size x l
     # so the vector gather works
     emission_probs = emission_probs.transpose(1, 2)
+    dp = dp.to(transition_matrix.device)
     for i in range(1, m):
         dp[:, i, :] = vector_gather(emission_probs, targets[:, i]) + (torch.logsumexp(dp[:, i-1, :].unsqueeze(1).transpose(1, 2) + transition_matrix, dim=1))
     return dp
